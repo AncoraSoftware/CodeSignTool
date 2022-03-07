e9e51844d4c04cabd1e48f181af5d2df0713d86c
Sam Goldmann <samuel.goldmann@ancorasoftware.com>
2022-03-07 12:22:56 -0500

    Initial commit

---
+++ src/src/CodeSignToolTask/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Ancora.Tasks
+{
+
+	public class CodeSignTool : ContextAwareTask
+	{	protected override bool ExecuteInner()
+		{
+			
+		}
+	}
+}
